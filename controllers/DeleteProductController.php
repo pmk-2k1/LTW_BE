72bac154f8e2eae93d1f82ff0cc438b7e3b87c28
Tiến Lộc <tienlocak14@gmail.com>
2023-04-24 08:14:05 +0700

    edit file deleteProductController.php

--- controllers/DeleteProductController.php
+++ controllers/DeleteProductController.php
@@ -1,9 +1,8 @@
 <?php
 require_once '..\config\database.php';
-require_once '..\models\Product.php';
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET POST DELETE");
+header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
 header("Content-type:application/json");
 
@@ -14,21 +13,19 @@ if($data == null){
     echo "{'Error':'Can not get data from UI'}";
     exit;
 }
-$name= $data->productName;
-
 $db = new Database();
-$query = "SELECT Id FROM product WHERE Name = '$name'";
-$deleteCheck = $db->query($query);
-$data_id =  $deleteCheck->fetch(PDO::FETCH_ASSOC);
+
+$select_id = $db->query("SELECT * FROM product WHERE id = '$data->productId'");
+$data_id = $select_id->fetch(PDO::FETCH_ASSOC);
 if($data_id == false) {
+    echo '{"isSuccess": "false", "error": "Can not find user with id: '.$data->productId.'"}';
     exit;
 }
-$id =  $data_id['Id'];
 try{
-    $sql = "DELETE FROM product WHERE Id = '$id'";
-    $database = $db->query($sql);
-}catch (Exception $e) {
-    echo 'Error with: ' .$e->getMessage();
+    $sql = "DELETE FROM product WHERE Id = '$data->productId'";
+    $deleteProduct = $db->query($sql);
+    // $delete = $deleteProduct->fetch(PDO::FETCH_ASSOC);
+    echo '{"isSuccess": "true", "error": null}';
+}catch(Exception $e){
+    echo '{"isSuccess": "false", "error": "Can not find user with id: '.$e->getMessage().'"}';
 }
-
-// echo json_encode($database->fetch(PDO::FETCH_ASSOC));
