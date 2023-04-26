760f7791fef67f4c0fb387f00db33be3173c224d
Tiến Lộc <tienlocak14@gmail.com>
2023-04-27 01:20:57 +0700

    Update add product

--- controllers/AddProductController.php
+++ controllers/AddProductController.php
@@ -17,7 +17,8 @@ if ($data == null) {
 $db = new Database();
 
 try {
-    $product = new Product(generate_uuid(), $data->isActive, $data->name, $data->type, $data->price, $data->quantity, $data->description);
+    $product = new Product(generate_uuid(), $data->isActive, $data->name, $data->type, $data->price, $data->quantity, $data->description,
+            $data->material, $data->style, $data->album, $data->model, $data->connect);
     $productData = $product->returnProductArray();
 
     $query = "SELECT * FROM product WHERE Type = '$productData[productType]' AND Name = '$productData[productName]'";
@@ -27,9 +28,10 @@ try {
         exit;
     }
 
-    $db->query("INSERT INTO product(Id, Is_active, Name, Type, Price, Quantity, Description)
+    $db->query("INSERT INTO product(Id, Is_active, Name, Type, Price, Quantity, Description, Material, Style, Album, Model, Connect)
     VALUES ('" .$productData['productId']. "','" .$productData['productIsActive']. "','" .$productData['productName']. "','" .$productData['productType']. "','"
-    .$productData['productPrice']. "','" .$productData['productQuantity']. "','" .$productData['productDescription']. "')");
+    .$productData['productPrice']. "','" .$productData['productQuantity']. "','" .$productData['productDescription']. "','" .$productData['material']. "','"
+    .$productData['style']. "','" .$productData['album']. "','" .$productData['model']. "','" .$productData['connect']. "')");
     echo '{"isSuccess": true, "message": "Thêm sản phẩm thành công"}';
 } catch (Exception $e) {
     echo '{"isSuccess": false , "message": "Error SQL: '.$e->getMessage().'"}';
--- models/Product.php
+++ models/Product.php
@@ -8,10 +8,26 @@ class Product
     private $productPrice;
     private $productQuantity;
     private $productDescription;
+    private $material;
+    private $style;
+    private $album;
+    private $model;
+    private $connect;
 
-
-    public function __construct($productId, $productIsActive, $productName, $productType, $productPrice, $productQuantity, $productDescription)
-    {
+    public function __construct(
+        $productId,
+        $productIsActive,
+        $productName,
+        $productType,
+        $productPrice,
+        $productQuantity,
+        $productDescription,
+        $material,
+        $style,
+        $album,
+        $model,
+        $connect
+    ) {
         $this->productId = $productId;
         $this->productIsActive = $productIsActive;
         $this->productName = $productName;
@@ -19,8 +35,13 @@ class Product
         $this->productPrice = $productPrice;
         $this->productQuantity = $productQuantity;
         $this->productDescription = $productDescription;
-
+        $this->$material = $material;
+        $this->$style = $style;
+        $this->$album = $album;
+        $this->$model = $model;
+        $this->$connect = $connect;
     }
+
     public function getProductID()
     {
         return $this->productId;
@@ -50,6 +71,31 @@ class Product
         return $this->productDescription;
     }
 
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+
+    public function getStyle()
+    {
+        return $this->style;
+    }
+
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function getConnect()
+    {
+        return $this->connect;
+    }
+
     public function returnProductArray()
     {
         $product = array();
@@ -60,6 +106,11 @@ class Product
         $product['productPrice'] = $this->productPrice;
         $product['productQuantity'] = $this->productQuantity;
         $product['productDescription'] = $this->productDescription;
+        $product['material'] = $this->material;
+        $product['style'] = $this->style;
+        $product['album'] = $this->album;
+        $product['model'] = $this->model;
+        $product['connect'] = $this->connect;
         return $product;
     }
 }
