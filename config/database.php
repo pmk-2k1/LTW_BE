70d926e690f72e7a17f7b1c788c05b53f46b4e3a
Tiến Lộc <tienlocak14@gmail.com>
2023-04-28 12:09:19 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -10,8 +10,9 @@ class Database extends PDO
     {
         try {
             $dsn = "mysql:host=$this->host;dbname=$this->database;charset=UTF8";
-            parent::__construct($dsn, $this->user, '',
+            parent::__construct($dsn, $this->user, $this->password,
             array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch (PDOException $e) {
